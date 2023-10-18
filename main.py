5a3972eebf3c20ca8ac31c1fcfc74d235db72c99
Gary Rouch <85609262+garyrouch@users.noreply.github.com>
2023-10-18 17:50:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import streamlit as st
 # Titre de la page
 st.title("Notre Site est en construction...")
 
-# Bouton "Nous Contacter" qui redirige vers le lien Calendly
-url = "https://calendly.com/jonathan-yana-qolaig"
-st.link_button("Nous Contacter", url = url, type = "primary")
+# Centre le contenu en utilisant une colonne
+col1, col2, col3 = st.columns([1, 3, 1])
+
+# Le bouton "Nous Contacter" se trouve dans la colonne du milieu (col2)
+with col2:
+  # Bouton "Nous Contacter" qui redirige vers le lien Calendly
+  url = "https://calendly.com/jonathan-yana-qolaig"
+  st.link_button("Nous Contacter", url = url, type = "primary")
