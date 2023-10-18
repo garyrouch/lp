dd5652c232e4bc58880efdf550d6793134420282
Gary Rouch <85609262+garyrouch@users.noreply.github.com>
2023-10-18 17:45:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import webbrowser
 
 # Titre de la page
 st.title("Notre Site est en construction...")
 
 # Bouton "Nous Contacter" qui redirige vers le lien Calendly
-if st.button("Nous Contacter"):
-    url = "https://calendly.com/jonathan-yana-qolaig"
-    webbrowser.open_new_tab(url)
+url = "https://calendly.com/jonathan-yana-qolaig"
+st.link_button("Nous Contacter", url = url)
